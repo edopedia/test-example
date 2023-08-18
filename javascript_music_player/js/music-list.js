da1ef981d54adc27a8a0414ec17d73defa0d829b
Muhammad Furqan Ul Haq <22556762+trulyfurqan@users.noreply.github.com>
2023-08-18 20:19:57 +0500

    Added: JavaScript Music Player

---
+++ javascript_music_player/js/music-list.js
@@ -0,0 +1,38 @@
+let allMusic = [
+  {
+    name: "Faded",
+    artist: "Alan Walker",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Sugar & Brownies",
+    artist: "DHARIA",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Safari",
+    artist: "Serena",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Danza Kuduro",
+    artist: "Don Omar",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Ya Lili",
+    artist: "Balti ft. Hamouda",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Hislerim",
+    artist: "Serhat Durmus",
+    img: "music-6",
+    src: "music-6"
+  }
+];
